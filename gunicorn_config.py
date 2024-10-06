aa9c3ff18272e2a167f47afdea64ba35c890e509
igor20192 <igor.udovenko2012@meta.ua>
2024-10-06 23:40:33 +0300

    edit nginx.conf

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,5 +2,5 @@ bind = "0.0.0.0:8080"
 workers = 4
 
 
-certfile = "/etc/letsencrypt/live/shoppingtour.click/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/shoppingtour.click/privkey.pem"
+certfile = "/etc/letsencrypt/live/evgenykozhukhov.com/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/evgenykozhukhov.com/privkey.pem"
