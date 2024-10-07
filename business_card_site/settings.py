06f9db436cb83557b14684ae4dbce7fceb37f6a3
igor20192 <igor.udovenko2012@meta.ua>
2024-10-07 13:53:49 +0300

    settings.py edit ALLOWED_HOST

--- business_card_site/settings.py
+++ business_card_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS").split()
 
 
 # CSRF_TRUSTED_ORIGINS = [config("CSRF_TRUSTED_ORIGINS")]
