cc0cf3c9ac9c2ef30cabe63179d872ccb7cb10f0
igor20192 <igor.udovenko2012@meta.ua>
2024-10-08 18:03:34 +0300

    delete staticfiles

--- staticfiles/js/start_effect(version).js
+++
@@ -1,8 +0,0 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-ScrollSmoother.create({
-    smooth: 1,   
-    effects: true,
-  });
-  gsap.fromTo('.item-img', {marginTop: "150vh"}, {marginTop: "-105vh",duration: 1.7,ease: "expo.inOut",stagger: 0.05, });
-gsap.fromTo('.items__video',{width:"27vw",height:"29vh",top:'140vh'},{width:"100vw",height:"100vh",top:'105vh',duration:1,ease: "expo.out" ,delay:2.3})
-gsap.fromTo('.items__video',{width:"30vw",height:"30vh"},{width:"27vw",height:"29vh",duration:.7,ease: "expo.inOut" ,delay:1.5})
